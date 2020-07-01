553fa5b5c5b7774679d471cf6efe64f3194bb0a3
RenDeveloper <duncansrendall@gmail.com>
2020-07-01 21:09:57 +0100

    added rudimentary movement to player

---
+++ CraftkinGame/Assets/Scripts/playerMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    public float speed = 5.7f;
+
+    Vector3 playerInput;
+
+    Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+
+        PlayerInput();
+
+
+        
+    }
+
+    private void FixedUpdate()
+    {
+        Movement();
+    }
+
+    private void PlayerInput()
+    {
+
+        playerInput.x = Input.GetAxisRaw("Horizontal");
+        playerInput.z = Input.GetAxisRaw("Vertical");
+
+        playerInput.Normalize();
+
+    }
+
+    private void Movement()
+    {
+        rb.MovePosition(rb.position + playerInput * speed * Time.fixedDeltaTime);
+    }
+}

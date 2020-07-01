58722a96921235753a66dffe68de340d563d1ca8
RenDeveloper <duncansrendall@gmail.com>
2020-07-01 22:29:19 +0100

    added smooth movement over slopes

--- CraftkinGame/Assets/Scripts/playerMovement.cs
+++ CraftkinGame/Assets/Scripts/playerMovement.cs
@@ -9,38 +9,49 @@ public class playerMovement : MonoBehaviour
     Vector3 playerInput;
 
     Rigidbody rb;
-
-    private void Awake()
-    {
-        rb = GetComponent<Rigidbody>();
-    }
-
-    private void Update()
-    {
-
-        PlayerInput();
+    
 
 
-        
-    }
+    private void Awake() => rb = GetComponent<Rigidbody>();
 
-    private void FixedUpdate()
-    {
-        Movement();
-    }
+
+    private void Update() => PlayerInput();
+
+
+    private void FixedUpdate() => Movement();
+
+
+
 
     private void PlayerInput()
     {
-
+        //Gets the Input values of each access
         playerInput.x = Input.GetAxisRaw("Horizontal");
         playerInput.z = Input.GetAxisRaw("Vertical");
 
+        //Makes sure the Vector3 has a magnitude of 1  (prevents diagonal movement being faster than other directions)
         playerInput.Normalize();
 
     }
 
+    //Moves the Player
     private void Movement()
     {
-        rb.MovePosition(rb.position + playerInput * speed * Time.fixedDeltaTime);
+        rb.MovePosition(rb.position + getGroundNormalRotation() * playerInput * speed * Time.fixedDeltaTime);
+    }
+
+    //used to calculate what direction force should be applied to during movement (this is to make traversing slopes smoother)
+    private Quaternion getGroundNormalRotation()
+    {
+        RaycastHit hit;
+        Vector3 groundNormal;
+        
+        //sends a raycast at the ground and stores it in hit
+        Physics.Raycast(rb.position, -Vector3.up, out hit);
+        //gets the normal of the ground  aka which way the ground is facing
+        groundNormal = hit.normal;
+
+        //returns the angle from transform.up to the direction the normal is facing
+        return Quaternion.FromToRotation(transform.up, groundNormal);
     }
 }
